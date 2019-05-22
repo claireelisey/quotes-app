212a26bd29bfe04671794feb4c7c024f929b7db8
Claire Ouellette <claireelisey@gmail.com>
2019-05-22 16:49:33 -0500

    initial commit - backend / express app built

---
+++ controllers/quote-controller.js
@@ -0,0 +1,44 @@
+// Import the Quote model.
+const Quote =  require('../models/quote');
+
+// Instantiate the controller object
+const quoteController = {};
+
+// Controller method for handling a request for all quotes
+quoteController.findAll = (req, res) => {
+    // Uses the findAll method from Quote
+    Quote.findAll()
+    .then(quotes => {
+        // Sends the quotes as a JSON object
+        res.json({
+        message: 'Success',
+        data: quotes
+        });
+    })
+    .catch(err => {
+        // If something goes wrong it logs the error in the console and sends it as a JSON object
+        console.log(err);
+        res.status(500).json({err});
+    });
+};
+
+// Controller method for handling a request for a single quote
+quoteController.findById = (req, res) => {
+    // Quotes method for finding by id, passes the id as an argument
+    Quote.findById(req.params.id)
+    .then(quote => {
+        // Sends the quote as a JSON object
+        res.json({
+        message: "Success",
+        data: quote
+        });
+    })
+    .catch(err => {
+        // If something goes wrong it logs the error in the console and sends it as a JSON object
+        console.log(err);
+        res.status(500).json({err});
+    });
+};
+
+// Export the controller
+module.exports = quoteController;
